35d9911ad4ccf0f652d3a7caef37da5e7e6f786e
ResToEons <xtrakr@naver.com>
2024-01-22 06:34:41 +0900

    Create 8 - A+B - 8 (11022).cpp

---
+++ 3 - 반복문/8 - A+B - 8 (11022)/8 - A+B - 8 (11022).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int num;
+	int A, B;
+
+	std::cin >> num;
+
+	for (int i = 1; num >= i; i++)
+	{
+		std::cin >> A >> B;
+
+		if (A < 0 || B >= 10)
+			return -1;
+
+		std::cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << std::endl;
+	}
+
+	return 0;
+}
